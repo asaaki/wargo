25e9cb86d69763be1b0b50dff664939750686ea0
Christoph Grabo <asaaki@mannaz.cc>
2022-02-23 19:27:15 +0100

    Make clippy happy again
    I think I need a clippy for vscode. 😬

--- src/progress.rs
+++ src/progress.rs
@@ -2,7 +2,8 @@ use indicatif::{ProgressBar, ProgressStyle};
 
 pub(crate) fn bar(len: u64) -> ProgressBar {
     let style = ProgressStyle::default_bar()
-        .template("{msg:.green.bold} {wide_bar:.green/blue} {pos}/{len:.bold}").expect("template issue")
+        .template("{msg:.green.bold} {wide_bar:.green/blue} {pos}/{len:.bold}")
+        .expect("template issue")
         .progress_chars("█▒░");
 
     ProgressBar::new(len)
