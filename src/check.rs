9415c6b3d44a96ca3bc8695961399083905bbe7f
Christoph Grabo <asaaki@mannaz.cc>
2023-12-12 04:50:12 +0100

    Clean up wsl checker logic

--- src/check.rs
+++ src/check.rs
@@ -1,11 +1,10 @@
 // https://github.com/Dentosal/wsl-rs
 // but with explicit v2 check - we don't care about WSL1
 
-#[allow(dead_code)]
-const OS_RELEASE: &str = "/proc/sys/kernel/osrelease";
+use std::sync::OnceLock;
 
 pub(crate) fn wsl2_or_exit() -> crate::NullResult {
-    if is_not_wsl2() {
+    if !_is_wsl2() {
         return Err(std::io::Error::new(
             std::io::ErrorKind::Unsupported,
             "This command can only be used in a WSL2 environment",
@@ -15,23 +14,23 @@ pub(crate) fn wsl2_or_exit() -> crate::NullResult {
     Ok(())
 }
 
-fn is_not_wsl2() -> bool {
-    !is_wsl2()
+pub(crate) fn is_wsl2() -> bool {
+    static IS_WSL2: OnceLock<bool> = OnceLock::new();
+    *IS_WSL2.get_or_init(_is_wsl2)
 }
 
 #[cfg(target_os = "linux")]
-fn is_wsl2() -> bool {
-    if let Ok(b) = std::fs::read(OS_RELEASE) {
-        if let Ok(s) = std::str::from_utf8(&b) {
-            let a = s.to_ascii_lowercase();
-            return a.contains("wsl2");
-        }
-    }
-    false
+fn _is_wsl2() -> bool {
+    const OS_RELEASE: &str = "/proc/sys/kernel/osrelease";
+    const MARKER: &str = "wsl2";
+
+    std::fs::read_to_string(OS_RELEASE)
+        .map(|os_release| os_release.to_ascii_lowercase().contains(MARKER))
+        .unwrap_or_default()
 }
 
 /// Test if the program is running under WSL
 #[cfg(not(target_os = "linux"))]
-fn is_wsl2() -> bool {
+fn _is_wsl2() -> bool {
     false
 }
